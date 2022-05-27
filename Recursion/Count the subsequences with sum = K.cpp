03232c3e9871d984a8eb7baa89862ee293932518
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-05-27 12:47:23 +0530

    Create Count the subsequences with sum = K.cpp

---
+++ Recursion/Count the subsequences with sum = K.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int printS(int ind , int s, int sum, int arr[] , int n) {
+	if (ind == n) {
+		//condtion satisfied
+		if (s == sum) return 1;
+		//condtion not satisfied
+		else return 0;
+	}
+	//take or pick particular index into  the subsequence
+
+	s += arr[ind];
+
+	int l = printS(ind + 1, s, sum, arr, n);
+
+
+	s -= arr[ind];
+
+	//not take
+	int r = printS(ind + 1, s, sum, arr, n);
+
+	return l + r;
+}
+
+
+int main() {
+  // your code goes here
+
+	int arr[] = {1, 2, 1};
+	int n = 3;
+	int sum = 2;
+	cout << printS(0, 0, sum, arr, n);
+
+
+	return 0;
+}
+
+
+
+
+
+
