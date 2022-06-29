ee0011dff249aa0054838bce1b6eba4a38b227ff
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-06-29 21:24:22 +0530

    Create Search an element k in an array (descending order).cpp

---
+++ Binary Search/Search an element k in an array (descending order).cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(int arr[], int k, int start, int end) {
+	while (start <= end) {
+		int mid = start + (end - start) / 2;
+
+		if (k == arr[mid])
+			return mid;
+
+		else if (k < arr[mid])
+			start = mid + 1;
+
+		else
+			end = mid - 1;
+	}
+	return -1;
+}
+
+int main() {
+
+	// your code goes here
+	int n ;
+	cin >> n;
+
+	int arr[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	int k;
+	cin >> k;
+
+	int result = binarySearch(arr, k, 0, n - 1);
+	if (result == -1)
+		cout << "Not found";
+	else
+		cout << result << endl;
+}
+
+
+
+
+
+
