6a541667fd9219fb159ba68dd07ab149bfb6e8ce
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-07-06 23:47:40 +0530

    Update Find an element in a rotated sorted array.cpp

--- Binary Search/Find an element in a rotated sorted array.cpp
+++ Binary Search/Find an element in a rotated sorted array.cpp
@@ -56,3 +56,32 @@ int main()
 
 // O/P -> Index: 8
  
+
+//CLEANER CODE 
+int search(vector<int>& nums, int target) {
+        int start=0, end=nums.size()-1;
+        
+        while(start<=end)
+        {
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+            
+            if(nums[start]<=nums[mid])
+            {
+                if(nums[start]<=target && nums[mid]>=target)
+                    end = mid-1;
+                else
+                    start =mid+1;
+            }
+            else
+            { 
+                if(nums[mid]<=target && nums[end]>=target)
+                    start = mid+1;
+                else
+                    end = mid-1;
+            }
+        }
+        return -1;
+    }
