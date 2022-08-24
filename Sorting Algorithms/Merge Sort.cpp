6661581908962cc1755998f64e22657d97829424
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-08-24 10:29:29 +0530

    Create Merge Sort.cpp

---
+++ Sorting Algorithms/Merge Sort.cpp
@@ -0,0 +1,83 @@
+// TC -> O(nlogn)
+// SC -> O(n)
+// AS -> O(n)
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[], int l, int mid , int r) {
+
+	int i = l;         // starting index of left half of arr
+	int j = mid + 1;   // starting index of right half of arr
+	int f = l;         // index used to transfer elements in temporary array
+	int temp[100000];  // temporary array
+
+  //storing elements in the temporary array in a sorted manner//
+  
+	while (i <= mid && j <= r) {
+		if (arr[i] < arr[j]) {
+			temp[f] = arr[i];
+			f++, i++;
+		} 
+    else {
+			temp[f] = arr[j];
+			f++, j++;
+		}
+	}
+  
+  // if elements on the left half are still left //
+
+	if (i > mid) { //
+		while (j <= r) {
+			temp[f] = arr[j];
+			f++, j++;
+		}
+	}
+	else {
+    //  if elements on the right half are still left //
+		while (i <= mid) {
+			temp[f] = arr[i];
+			f++; i++;
+		}
+	}
+
+  // transfering all elements from temporary to arr //
+	for (int f = l; f <= r; f++) {
+		arr[f] = temp[f];
+	}
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+	if (l < r) {
+		int mid = (l + r) / 2;
+		mergeSort(arr, l, mid);     // left half
+		mergeSort(arr, mid + 1, r); // right half
+		merge(arr, l, mid, r);      // merging sorted halves
+	}
+}
+
+int main() {
+
+  // your code goes here
+	int arr[] = {5, 4, 6, 3, 2, 1};
+	int n = 6;
+	mergeSort(arr, 0, n - 1);
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
