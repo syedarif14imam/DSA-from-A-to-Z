a61bd539d710aeaf3fc6c4712b813a20d0acdc21
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-09-09 13:16:48 +0530

    Create Find missing and repeating element in the array.cpp

---
+++ Arrays/Find missing and repeating element in the array.cpp
@@ -0,0 +1,75 @@
+OPTIMAL APPROACH -> USING XOR METHOD 
+
+LOGIC->
+  1. xor all arr[i] - x
+  2. x ^ (1^2^3^......^n)
+  3. x ^ y = num
+  4. seperate in 2 baskets
+  5. seperate ( 1...n) in 2 baskets
+  6. xor both baskets to find the number.
+  
+// TC -> O (N)
+// SC -> O(1) As we are NOT USING EXTRA SPACE
+  
+  vector < int >Solution::repeatedNumber (const vector < int >&arr) {
+    /* Will hold xor of all elements and numbers from 1 to n */
+    int xor1;
+
+    /* Will have only single set bit of xor1 */
+    int rsbm;
+
+    int i;
+    int x = 0; // missing
+    int y = 0; // repeated
+    int n = arr.size();
+
+    xor1 = arr[0];
+
+    /* Get the xor of all array elements */
+    for (i = 1; i < n; i++)
+        xor1 = xor1 ^ arr[i];
+
+    /* XOR the previous result with numbers from 1 to n */
+    for (i = 1; i <= n; i++)
+        xor1 = xor1 ^ i;
+
+    /* Get the rightmost set bit in set_bit_no */
+    rsbm = xor1 & -xor1;
+
+    /* Now divide elements into two sets by comparing a rightmost set bit
+       of xor1 with the bit at the same position in each element.
+       Also, get XORs of two sets. The two XORs are the output elements.
+       The following two for loops serve the purpose */
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] & rsbm)
+            /* arr[i] belongs to first set */
+            x = x ^ arr[i];
+
+        else
+            /* arr[i] belongs to second set */
+            y = y ^ arr[i];
+    }
+
+    for (i = 1; i <= n; i++) {
+        if (i & rsbm)
+            /* i belongs to first set */
+            x = x ^ i;
+
+        else
+            /* i belongs to second set */
+            y = y ^ i;
+    }
+
+    // NB! numbers can be swapped, maybe do a check ?
+    int x_count = 0;
+    for (int i=0; i<n; i++) {
+        if (arr[i]==x)
+            x_count++;
+    }
+    
+    if (x_count==0)
+        return {y, x};
+    
+    return {x, y};
+}
