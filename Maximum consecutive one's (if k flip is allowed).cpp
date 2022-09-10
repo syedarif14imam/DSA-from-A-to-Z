035b0db6460139e44fcc2183a9d633b085e94f6e
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-09-10 10:10:18 +0530

    Create Maximum consecutive one's (if k flip is allowed).cpp

---
+++ Maximum consecutive one's (if k flip is allowed).cpp
@@ -0,0 +1,27 @@
+// TC-> O(N) 
+// SC-> O(1)
+
+     // k is maximum of number zeroes allowed to flip
+    // n is size of array
+    
+int findZeroes(int arr[], int n, int k) {
+        // code here
+        int maxconsecutiveones= 0;
+        int start=0;
+        int zerocount=0;
+        
+        for(int end=0;end<n;end++){
+            if(arr[end]==0){
+                zerocount++;
+            }
+            while(zerocount>m){
+                if(arr[start]==0){
+                    zerocount--;
+                }
+                start++;
+            }
+            maxconsecutiveones = max(maxconsecutiveones,end-start+1);
+            
+        }
+        return maxconsecutiveones;
+    }  
