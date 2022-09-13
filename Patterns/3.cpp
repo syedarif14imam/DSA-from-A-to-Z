785d455f240da1a2f9132c41cdc014f919cef2b0
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-09-13 18:28:52 +0530

    Create 3.cpp

---
+++ Patterns/3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+  
+{
+  int n = 5;
+	for(int i=1;i<=n;i++)
+  {
+		for (int j = 1; j <= n; j++) 
+    {
+		    if(j<= 6-i)
+        {
+		       cout << "*";
+		    }else{
+		        cout << " ";
+		    }
+			
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+OUTPUT->
+*****
+**** 
+***  
+**   
+*    
+
