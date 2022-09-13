38230b59869b24f5cce952ecfee0db0961a41f7b
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-09-13 22:17:23 +0530

    Create 5.cpp

---
+++ Patterns/5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+  
+{
+    int n = 5;
+	for(int i=1;i<=n;i++)
+	{
+		for (int j = 1; j <= i; j++) 
+		{
+		        cout << i;
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+OUTPUT->
+1
+22
+333
+4444
+55555
+
