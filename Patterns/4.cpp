98575ac7c98325346a9f442a0d7444990c3e14a1
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-09-13 21:17:26 +0530

    Create 4.cpp

---
+++ Patterns/4.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n = 5;
+	for(int i=1;i<=n;i++)
+  {
+		for (int j = 1; j <= n; j++) 
+    {
+		    if(j<=i)
+        {
+		       cout << j;
+		    }else{
+		        cout << " ";
+		    }
+			
+		}
+		cout << endl;
+	}
+	return 0;
+}
+
+OUTPUT->
+1    
+12   
+123  
+1234 
+12345
+
+
+
