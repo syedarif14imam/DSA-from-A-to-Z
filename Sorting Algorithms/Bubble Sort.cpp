f1e1b2c761d62adc9128911426ada4caa5ad50d5
SYED <81295030+syedarif14imam@users.noreply.github.com>
2022-10-20 21:54:09 +0530

    Update Bubble Sort.cpp

--- Sorting Algorithms/Bubble Sort.cpp
+++ Sorting Algorithms/Bubble Sort.cpp
@@ -6,9 +6,8 @@
 using namespace std;
 
 void BubbleSort(int arr[], int n) {
-	for (int i = 0; i < n - 1; i++)
+	for (int i = 0; i < n ; i++)
 	{
-		bool flag = 0;
 		for (int j = 0; j < n - i - 1; j++)
 		{
 			if (arr[j] > arr[j + 1]) 
@@ -16,11 +15,8 @@ void BubbleSort(int arr[], int n) {
 				int temp = arr[j];
 				arr [j] = arr [j + 1];
 				arr [j + 1] = temp;
-				bool flag = 1;
 			}
 		}
-		if (flag == 0)
-			break;
   }
 }
 
