42a1c9266aaa6e45386dac8d346b2bab4be36831
SYED <81295030+syedarif14imam@users.noreply.github.com>
2023-07-11 22:15:07 +0530

    Create Reverse a number.cpp

---
+++ Beginning with Programming/Loops/Reverse a number.cpp
@@ -0,0 +1,28 @@
+// 1st method using header file
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+   int n;
+   cin >> n;
+   
+   // compute no. of digits
+   int d=0, temp=n;
+   while(temp){
+       d++;
+       temp=temp/10;
+   }
+   // compute the ans 
+   int ans = 0;
+   while(n){
+       int r = n%10;
+       ans += r* pow(10,d-1);   // decreement d (no of digits)
+       d--;
+       n/=10;
+   }
+   cout << ans ;
+  return 0;
+}
